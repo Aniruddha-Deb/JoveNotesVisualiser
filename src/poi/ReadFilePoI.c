950aba3605e2c76c5b95d336065211044e2494df
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2017-11-13 13:19:42 +0530

    Working on a parser without lex and yacc

--- src/poi/ReadFilePoI.c
+++ src/poi/ReadFilePoI.c
@@ -17,16 +17,42 @@ void process_definition( char* question, char* definition ) {
 }
 
 int main( void ) {
+
     printf("Running\n");
-    extern FILE* yyin;
+
+    FILE* fp = fopen( "/Users/Sensei/JoveNotes/JoveNotes-X/Class-10/Physics/12 - Thermionic Emission/12.1 - Thermionic Emission (qa).jn", "r" );
     FILE* fout = fopen( "/Users/Sensei/temp/cout.txt", "w" );
-    yyin = fopen( "/Users/Sensei/JoveNotes/JoveNotes-X/Class-10/Physics/12 - Thermionic Emission/12.1 - Thermionic Emission (qa).jn", "r" );
-    if( yyin == NULL ) {
+
+    if( fp == NULL ) {
         printf("Could not open file\n");
         return -1;
     }
-    yyparse();
-    fclose( yyin );
+
+    char tempChar;
+    while( (tempChar=fgetc( fp )) != EOF ) {
+        if( (int)tempChar <= 32 ) {
+            // whitespace: do nothing
+        }
+        else if( tempChar == '\"' ) {
+            char *str = malloc( 2 * sizeof( char ) );
+            str[0] = tempChar;
+            str[1] = '\0';
+
+            while( (tempChar=fgetc( fp )) ) {
+                int len = strlen( str );
+                str = realloc( str, (len+2)*sizeof( char ) );
+                str[len] = tempChar;
+                str[len+1] = '\0';
+                if( str[len] == '\"' && str[len-1] != '\\' ) {
+                    fprintf(fout, "%s", str );
+                    break;                    
+                }
+            }
+            free( str );
+        }
+    }
+
+    fclose( fp );
     fclose( fout );
 
     return 0;
